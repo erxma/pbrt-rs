071dbc2c1bd6159ad6c4ec2d739eeb6ece8b47c5
Eric <>
2023-08-27 16:44:48 -0500

    Impl more Tuple methods, unify Ord and Float fns with PartialOrd, relax T bound on Tuple to Copy

--- src/geometry/vec3.rs
+++ src/geometry/vec3.rs
@@ -1,13 +1,10 @@
-use std::ops::{Div, Index, IndexMut, Neg};
+use std::ops::{Div, Index, IndexMut};
 
-use num_traits::{Float, NumCast, Signed};
+use num_traits::{NumCast, Signed};
 
 use crate::{self as pbrt, impl_tuple_math_ops_generic, math::routines::safe_asin, PI};
 
-use super::{
-    normal3::Normal3,
-    tuple::{Tuple, TupleElement},
-};
+use super::{normal3::Normal3, tuple::TupleElement};
 
 /// A 3D vector.
 #[derive(Clone, Copy, Debug, Default, PartialEq)]
@@ -25,54 +22,11 @@ impl<T> Vec3<T> {
     pub const fn new(x: T, y: T, z: T) -> Self {
         Self { x, y, z }
     }
-
-    /// Convert vector elements into another type.
-    pub fn into_<U>(self) -> Vec3<U>
-    where
-        T: Into<U>,
-    {
-        Vec3 {
-            x: self.x.into(),
-            y: self.y.into(),
-            z: self.z.into(),
-        }
-    }
-
-    /// Permute a vector's elements according to the index values
-    /// given.
-    pub fn permute(self, x: usize, y: usize, z: usize) -> Self
-    where
-        T: Copy,
-    {
-        Self {
-            x: self[x],
-            y: self[y],
-            z: self[z],
-        }
-    }
-}
-
-impl<T: TupleElement> Tuple<3, T> for Vec3<T> {
-    fn from_array(vals: [T; 3]) -> Self {
-        Self::new(vals[0], vals[1], vals[2])
-    }
 }
 
 impl_tuple_math_ops_generic!(Vec3; 3);
 
 impl<T: TupleElement> Vec3<T> {
-    /// Returns a vector with the absolute values of the components.
-    pub fn abs(self) -> Self
-    where
-        T: Signed,
-    {
-        Self {
-            x: self.x.abs(),
-            y: self.y.abs(),
-            z: self.z.abs(),
-        }
-    }
-
     /// The squared length of a vector.
     pub fn length_squared(self) -> T {
         self.dot(self)
@@ -93,20 +47,6 @@ impl<T: TupleElement> Vec3<T> {
             x: NumCast::from(v1y * v2x - v1z * v2y).unwrap(),
             y: NumCast::from(v1z * v2x - v1x * v2z).unwrap(),
             z: NumCast::from(v1x * v2y - v1y * v2x).unwrap(),
-        }
-    }
-
-    /// Returns the index of the component with the largest value.
-    pub fn max_dimension(self) -> usize
-    where
-        T: PartialOrd,
-    {
-        if self.x > self.y && self.x > self.z {
-            0
-        } else if self.y > self.x && self.y > self.z {
-            1
-        } else {
-            2
         }
     }
 
@@ -168,77 +108,6 @@ where
     }
 }
 
-impl<T: Float> Vec3<T> {
-    /// Returns true if any component is NaN.
-    pub fn has_nans(self) -> bool {
-        self.x.is_nan() || self.y.is_nan() || self.z.is_nan()
-    }
-}
-
-impl<T: Ord> Vec3<T> {
-    /// Returns the element with the smallest value.
-    pub fn min_component(self) -> T {
-        self.x.min(self.y).min(self.z)
-    }
-
-    /// Returns the element with the largest value.
-    pub fn max_component(self) -> T {
-        self.x.max(self.y).max(self.z)
-    }
-
-    /// Returns a vector containing the min values for each
-    /// component of `self` and `other` (the component-wise min).
-    pub fn min(self, other: Self) -> Self {
-        Self {
-            x: self.x.min(other.x),
-            y: self.y.min(other.y),
-            z: self.z.min(other.z),
-        }
-    }
-
-    /// Returns a vector containing the max values for each
-    /// component of `self` and `other` (the component-wise max).
-    pub fn max(self, other: Self) -> Self {
-        Self {
-            x: self.x.max(other.x),
-            y: self.y.max(other.y),
-            z: self.z.max(other.z),
-        }
-    }
-}
-
-impl<T: Float> Vec3<T> {
-    /// Returns the float element with the smallest value.
-    pub fn min_component_float(self) -> T {
-        self.x.min(self.y).min(self.z)
-    }
-
-    /// Returns the float element with the largest value.
-    pub fn max_component_float(self) -> T {
-        self.x.max(self.y).max(self.z)
-    }
-
-    /// Returns a vector containing the min float values for each
-    /// component of `self` and `other` (the component-wise min).
-    pub fn min_f(self, other: Self) -> Self {
-        Self {
-            x: self.x.min(other.x),
-            y: self.y.min(other.y),
-            z: self.z.min(other.z),
-        }
-    }
-
-    /// Returns a vector containing the max float values for each
-    /// component of `self` and `other` (the component-wise max).
-    pub fn max_f(self, other: Self) -> Self {
-        Self {
-            x: self.x.max(other.x),
-            y: self.y.max(other.y),
-            z: self.z.max(other.z),
-        }
-    }
-}
-
 impl<T> Index<usize> for Vec3<T> {
     type Output = T;
 
@@ -261,19 +130,6 @@ impl<T> IndexMut<usize> for Vec3<T> {
             1 => &mut self.y,
             2 => &mut self.z,
             _ => panic!("Index out of bounds for Vec3"),
-        }
-    }
-}
-
-impl<T: Neg<Output = T>> Neg for Vec3<T> {
-    type Output = Self;
-
-    /// Negate a vector's elements.
-    fn neg(self) -> Self {
-        Self {
-            x: -self.x,
-            y: -self.y,
-            z: -self.z,
         }
     }
 }

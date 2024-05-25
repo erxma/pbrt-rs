889ec01338556fd931400da6ba9f9269ba1b7c25
Eric <>
2024-05-25 00:27:46 -0500

    Rename f64 feature and bump dependencies

--- src/lib.rs
+++ src/lib.rs
@@ -9,22 +9,22 @@ pub mod shapes;
 pub mod util;
 
 // Choice of representation of floats
-#[cfg(feature = "float-as-double")]
+#[cfg(feature = "use-f64")]
 mod f64_float {
     pub type Float = f64;
     pub const MACHINE_EPSILON: Float = f64::EPSILON * 0.5;
     pub const PI: Float = std::f64::consts::PI;
 }
 
-#[cfg(feature = "float-as-double")]
+#[cfg(feature = "use-f64")]
 pub use f64_float::*;
 
-#[cfg(not(feature = "float-as-double"))]
+#[cfg(not(feature = "use-f64"))]
 mod f32_float {
     pub type Float = f32;
     pub const MACHINE_EPSILON: Float = f32::EPSILON * 0.5;
     pub const PI: Float = std::f32::consts::PI;
 }
 
-#[cfg(not(feature = "float-as-double"))]
+#[cfg(not(feature = "use-f64"))]
 pub use f32_float::*;

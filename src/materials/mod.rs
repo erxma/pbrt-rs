f9eb7e158187aa631c318e03ed130fcdf4c768a8
Eric <>
2024-08-06 14:12:38 -0500

    Start textures

--- src/materials/mod.rs
+++ src/materials/mod.rs
@@ -1,6 +1,11 @@
 mod mappings;
+mod textures;
 
 pub use mappings::{
     PointTransformMapping, TexCoord2D, TexCoord3D, TextureEvalContext, TextureMapping2D,
     TextureMapping2DEnum, TextureMapping3D, TextureMapping3DEnum, UvMapping,
+};
+pub use textures::{
+    ConstantFloatTexture, ConstantSpectrumTexture, FloatTexture, FloatTextureEnum, SpectrumTexture,
+    SpectrumTextureEnum,
 };
---
+++ src/materials/textures.rs
@@ -0,0 +1,79 @@
+use delegate::delegate;
+use enum_dispatch::enum_dispatch;
+
+use crate::{
+    sampling::spectrum::{SampledSpectrum, SampledWavelengths, SpectrumEnum},
+    Float,
+};
+
+use super::TextureEvalContext;
+
+#[enum_dispatch]
+pub enum FloatTextureEnum {
+    Constant(ConstantFloatTexture),
+}
+
+impl FloatTextureEnum {
+    delegate! {
+        #[through(FloatTexture)]
+        to self {
+            pub fn eval(&self, ctx: TextureEvalContext) -> Float;
+        }
+    }
+}
+
+#[enum_dispatch(FloatTextureEnum)]
+pub trait FloatTexture {
+    fn eval(&self, ctx: TextureEvalContext) -> Float;
+}
+
+#[enum_dispatch]
+pub enum SpectrumTextureEnum<'a> {
+    Constant(ConstantSpectrumTexture<'a>),
+}
+
+impl<'a> SpectrumTextureEnum<'a> {
+    delegate! {
+        #[through(SpectrumTexture)]
+        to self {
+            pub fn eval(&self, ctx: TextureEvalContext, lambda: &SampledWavelengths) -> SampledSpectrum;
+        }
+    }
+}
+
+#[enum_dispatch(SpectrumTextureEnum)]
+pub trait SpectrumTexture {
+    fn eval(&self, ctx: TextureEvalContext, lambda: &SampledWavelengths) -> SampledSpectrum;
+}
+
+pub struct ConstantFloatTexture {
+    value: Float,
+}
+
+impl FloatTexture for ConstantFloatTexture {
+    fn eval(&self, _ctx: TextureEvalContext) -> Float {
+        self.value
+    }
+}
+
+impl ConstantFloatTexture {
+    pub fn new(value: Float) -> Self {
+        Self { value }
+    }
+}
+
+pub struct ConstantSpectrumTexture<'a> {
+    value: SpectrumEnum<'a>,
+}
+
+impl SpectrumTexture for ConstantSpectrumTexture<'_> {
+    fn eval(&self, _ctx: TextureEvalContext, lambda: &SampledWavelengths) -> SampledSpectrum {
+        self.value.sample(lambda)
+    }
+}
+
+impl<'a> ConstantSpectrumTexture<'a> {
+    pub fn new(value: SpectrumEnum<'a>) -> Self {
+        Self { value }
+    }
+}

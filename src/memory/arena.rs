aa3dd199125b6a2d99806f09d82b6ba063c973ea
Eric <>
2024-08-07 15:54:59 -0500

    Implement ScratchBuffer using bumpalo

---
+++ src/memory/arena.rs
@@ -0,0 +1,36 @@
+use bumpalo::Bump;
+use crossbeam_utils::CachePadded;
+
+pub struct ScratchBuffer {
+    bump: CachePadded<Bump>,
+}
+
+impl ScratchBuffer {
+    pub fn new() -> Self {
+        Self {
+            bump: CachePadded::new(Bump::new()),
+        }
+    }
+
+    pub fn with_capacity(capacity: usize) -> Self {
+        Self {
+            bump: CachePadded::new(Bump::with_capacity(capacity)),
+        }
+    }
+
+    pub fn alloc<T>(&mut self, val: T) -> &mut T {
+        self.bump.alloc(val)
+    }
+
+    pub fn alloc_slice_clone<T: Clone>(&mut self, src: &[T]) -> &mut [T] {
+        self.bump.alloc_slice_clone(src)
+    }
+
+    pub fn alloc_slice_copy<T: Copy>(&mut self, src: &[T]) -> &mut [T] {
+        self.bump.alloc_slice_copy(src)
+    }
+
+    pub fn reset(&mut self) {
+        self.bump.reset()
+    }
+}

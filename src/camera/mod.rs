8461fbe08c55438659c3e5254d99f9d8bee224b2
Eric <>
2024-08-12 16:40:31 -0500

    Start implementing scene file parser

--- src/camera/mod.rs
+++ src/camera/mod.rs
@@ -1,8 +1,9 @@
 mod cameras;
-pub mod film;
+mod film;
 pub mod sensor;
 
 pub use cameras::{
     Camera, CameraRay, CameraRayDifferential, CameraSample, CameraTransform, OrthographicCamera,
     OrthographicCameraBuilder, OrthographicCameraBuilderError,
 };
+pub use film::{Film, RGBFilm, RGBFilmBuilder, RGBFilmBuilderError, VisibleSurface};

12ef0689032fa5cbcac3581800ab62b2f2584272
Eric <>
2024-08-15 14:50:30 -0500

    Implement Frame

--- src/geometry/mod.rs
+++ src/geometry/mod.rs
@@ -1,11 +1,13 @@
 mod bounds;
 mod direction_cone;
+mod frame;
 mod interaction;
 mod ray;
 mod transform;
 
 pub use bounds::{Bounds2f, Bounds2i, Bounds2iIterator, Bounds3f, Bounds3i};
 pub use direction_cone::DirectionCone;
+pub use frame::{Frame, FrameTransform};
 pub use interaction::{
     Interaction, InteractionCommon, IntraMediumInteraction, MediumInterfaceInteraction,
     SampleInteraction, Shading, SurfaceInteraction, SurfaceInteractionBuilder,

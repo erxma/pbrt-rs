e17ce5a26270fe3c87c541ba1b84065353f356a1
Eric <>
2024-08-16 13:01:42 -0500

    Impl Parallel2D, image_tile_render

---
+++ src/parallel/loops.rs
@@ -0,0 +1,34 @@
+use itertools::iproduct;
+use rayon::iter::{ParallelBridge, ParallelIterator};
+
+use crate::{geometry::Bounds2i, math::Point2i, Float};
+
+pub fn parallel_for_2d_with<T>(
+    extent: Bounds2i,
+    init: T,
+    op: impl (Fn(&mut T, Bounds2i)) + Send + Sync,
+) where
+    T: Send + Clone,
+{
+    let est_tile_size = (extent.diagonal().x() * extent.diagonal().y()) as usize
+        / (8 * rayon::current_num_threads());
+    let tile_size = ((est_tile_size as Float).sqrt() as usize).clamp(1, 32);
+
+    let tiles = iproduct!(
+        (extent.p_min.x()..=extent.p_max.x()).step_by(tile_size),
+        (extent.p_min.y()..=extent.p_max.y()).step_by(tile_size),
+    )
+    .map(|(x_start, y_start)| {
+        let tile_size = tile_size as i32;
+        let tile_start = Point2i::new(x_start, y_start);
+        let tile_end = Point2i::new(
+            (x_start + tile_size - 1).min(extent.p_max.x()),
+            (y_start + tile_size - 1).min(extent.p_max.y()),
+        );
+
+        Bounds2i::new(tile_start, tile_end)
+    });
+
+    // TODO: Confirm that par_bridge is sufficient; could collect into Vec first?
+    tiles.par_bridge().for_each_with(init, op);
+}
--- src/parallel/mod.rs
+++ src/parallel/mod.rs
@@ -1,3 +1,5 @@
 mod atomic;
+mod loops;
 
 pub use atomic::AtomicF64;
+pub use loops::parallel_for_2d_with;

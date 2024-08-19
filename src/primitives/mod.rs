813d6d6d8b5a0051435cc7f5268a7410962a5bd0
Eric <>
2024-08-19 16:55:31 -0500

    Make separate folder for primitives

---
+++ src/primitives/mod.rs
@@ -0,0 +1,24 @@
+use enum_dispatch::enum_dispatch;
+
+use crate::{
+    geometry::{Bounds3f, Ray},
+    shapes::ShapeIntersection,
+    Float,
+};
+
+mod single;
+
+pub use single::SimplePrimitive;
+
+#[enum_dispatch]
+pub enum PrimitiveEnum {
+    Simple(SimplePrimitive),
+}
+
+#[enum_dispatch(PrimitiveEnum)]
+pub trait Primitive {
+    fn bounds(&self) -> Bounds3f;
+    fn intersect<'a>(&'a self, ray: &'a Ray, t_max: Option<Float>)
+        -> Option<ShapeIntersection<'a>>;
+    fn intersect_p(&self, ray: &Ray, t_max: Option<Float>) -> bool;
+}
--- src/primitives/single.rs
+++ src/primitives/single.rs
@@ -1,7 +1,5 @@
 use std::sync::Arc;
 
-use enum_dispatch::enum_dispatch;
-
 use crate::{
     geometry::{Bounds3f, Ray},
     materials::Material,
@@ -9,18 +7,7 @@ use crate::{
     Float,
 };
 
-#[enum_dispatch]
-pub enum PrimitiveEnum {
-    Simple(SimplePrimitive),
-}
-
-#[enum_dispatch(PrimitiveEnum)]
-pub trait Primitive {
-    fn bounds(&self) -> Bounds3f;
-    fn intersect<'a>(&'a self, ray: &'a Ray, t_max: Option<Float>)
-        -> Option<ShapeIntersection<'a>>;
-    fn intersect_p(&self, ray: &Ray, t_max: Option<Float>) -> bool;
-}
+use super::Primitive;
 
 pub struct SimplePrimitive {
     // TODO: Cost of Arcs acceptable?

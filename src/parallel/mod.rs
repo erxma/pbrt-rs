a3bdbb9aa3044885287d2b52b6b2d26eb07857fd
Eric <>
2024-08-21 17:28:26 -0500

    Impl build_hlbvh

--- src/parallel/mod.rs
+++ src/parallel/mod.rs
@@ -2,4 +2,4 @@ mod atomic;
 mod loops;
 
 pub use atomic::AtomicF64;
-pub use loops::{join, parallel_for_2d_with};
+pub use loops::{join, parallel_for, parallel_for_2d_with, parallel_map, parallel_map_enumerate};

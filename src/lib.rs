b56894e43aca82dede0f8028ee1a6130f64df36c
Eric <>
2024-08-23 17:35:52 -0500

    Revert to reexporting individual Float consts (star import isn't shown by autocomplete

--- src/lib.rs
+++ src/lib.rs
@@ -20,8 +20,11 @@ pub mod util;
 #[cfg(feature = "use-f64")]
 pub mod float {
     use crate::Float;
-    pub use std::f64::consts::*;
     pub const MACHINE_EPSILON: Float = f64::EPSILON * 0.5;
+    pub const PI: Float = std::f64::consts::PI;
+    pub const SQRT_2: Float = std::f64::consts::SQRT_2;
+    pub const FRAC_PI_2: Float = std::f64::consts::FRAC_PI_2;
+    pub const FRAC_PI_4: Float = std::f64::consts::FRAC_PI_4;
     pub const INV_4_PI: Float = 0.07957747154594767;
 }
 
@@ -31,8 +34,11 @@ pub type Float = f64;
 #[cfg(not(feature = "use-f64"))]
 pub mod float {
     use crate::Float;
-    pub use std::f32::consts::*;
     pub const MACHINE_EPSILON: Float = f32::EPSILON * 0.5;
+    pub const PI: Float = std::f32::consts::PI;
+    pub const SQRT_2: Float = std::f32::consts::SQRT_2;
+    pub const FRAC_PI_2: Float = std::f32::consts::FRAC_PI_2;
+    pub const FRAC_PI_4: Float = std::f32::consts::FRAC_PI_4;
     pub const INV_4_PI: Float = 0.07957747;
 }
 
